bcfcd0f3b432868b840bc93ed52773cfda091385
Bystroushaak <bystrousak@kitakitsune.org>
2014-06-08 22:23:45 +0200

    Added brython and simple proof that it works.

---
+++ argparse_builder.py
@@ -0,0 +1,38 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# Interpreter version: python 2.7
+#
+#= Imports ====================================================================
+
+
+
+#= Variables ==================================================================
+
+
+
+#= Functions & objects ========================================================
+
+
+
+#= Main program ===============================================================
+from browser import document as doc
+
+doc["output"] <= "it is working"
+
+class ArgConfiguration(object):
+    def __init__(self, ):
+        pass
+
+def serialize_to_python(args):
+    """
+    Serializes dictionary with data to actual python code, which is then
+    displayed in output textarea.
+    """
+    pass
+
+def collect_data():
+    """
+    Collects data from all forms to the Configuration object.
+    """
+    pass
